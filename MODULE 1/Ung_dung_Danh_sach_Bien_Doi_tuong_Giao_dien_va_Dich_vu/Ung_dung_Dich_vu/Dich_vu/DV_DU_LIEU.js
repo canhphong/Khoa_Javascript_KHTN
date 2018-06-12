f834b59b7d827cc846c26034308c3c83ce657987
quangtt <quangtt@runsystem.net>
2018-06-13 06:34:09 +0700

    Ung dung Danh sach Bien Doi tuong co Giao dien va Dich vu

---
+++ MODULE 1/Ung_dung_Danh_sach_Bien_Doi_tuong_Giao_dien_va_Dich_vu/Ung_dung_Dich_vu/Dich_vu/DV_DU_LIEU.js
@@ -0,0 +1,36 @@
+var http = require("http");
+var Luu_tru = require("../Xu_ly/XL_LUU_TRU.js")
+var Xu_ly_Tham_so = require('querystring')
+var Port = 1000
+var Du_lieu = {}
+Du_lieu.Danh_sach_Dien_thoai = Luu_tru.Doc_Danh_sach("Dien_thoai")
+
+var Dich_vu = http.createServer(
+    (Yeu_cau, Dap_ung) => {
+        var Chuoi_Nhan = ""
+        var Chuoi_Kq = "{}"
+        var Dia_chi = Yeu_cau
+            .url
+            .replace("/", "").replace("?", "")
+        var Tham_so = Xu_ly_Tham_so.parse(Dia_chi)
+        var Ma_so_Xu_ly = Tham_so.Ma_so_Xu_ly
+        Yeu_cau.on('data', (chunk) => { Chuoi_Nhan += chunk })
+        Yeu_cau.on('end', () => {
+            if (Ma_so_Xu_ly == "Doc_Danh_sach_Dien_thoai") {
+                var Doi_tuong_Kq = Du_lieu.Danh_sach_Dien_thoai
+                Chuoi_Kq = JSON.stringify(Doi_tuong_Kq)
+                Dap_ung.setHeader("Access-Control-Allow-Origin", '*')
+                Dap_ung.end(Chuoi_Kq);
+            } else {
+                Chuoi_Kq = Luu_tru.Doc_Thong_tin_Dich_vu()
+                Dap_ung.setHeader("Access-Control-Allow-Origin", '*')
+                Dap_ung.end(Chuoi_Kq);
+            }
+
+        })
+
+    })
+Dich_vu.listen(Port,
+    console.log("Dịch vụ Dữ liệu đang thực thi ...: " + Port)
+);
+
---
+++ MODULE 1/Ung_dung_Danh_sach_Bien_Doi_tuong_Giao_dien_va_Dich_vu/Ung_dung_Dich_vu/Dich_vu/DV_MEDIA.js
@@ -0,0 +1,25 @@
+var http = require("http");
+var Luu_tru = require("../Xu_ly/XL_LUU_TRU.js")
+var Xu_ly_Tham_so = require('querystring')
+var Port = 1001
+var Dich_vu = http.createServer(
+    (Yeu_cau, Dap_ung) => {
+        var Chuoi_Nhan = ""
+        var Nhi_phan_Kq = ""
+        var Ten = Yeu_cau
+            .url
+            .replace("/", "")
+        Yeu_cau.on('data', (chunk) => { Chuoi_Nhan += chunk })
+        Yeu_cau.on('end', () => {
+            if (Ten != "") {
+                Nhi_phan_Kq = Luu_tru.Doc_Nhi_phan_Media(Ten)
+            }
+            Dap_ung.setHeader("Access-Control-Allow-Origin", '*')
+            Dap_ung.writeHead(200, { 'Content-Type': 'image/png' });
+            Dap_ung.end(Nhi_phan_Kq, 'binary');
+        })
+
+    })
+Dich_vu.listen(Port,
+    console.log("Dịch vụ Media đang thực thi ...: " + Port)
+);
